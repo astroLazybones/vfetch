c06e56df9e20a91e2b9fa09fffa2881060570ab8
Astro <148036040+astroLazybones@users.noreply.github.com>
2024-08-09 17:12:40 +1200

    Add files via upload

--- src/main.v
+++ src/main.v
@@ -3,13 +3,15 @@ module main
 import os
 
 fn main() {
-  uname := os.uname()
-  uname_version := uname.release
-  uname_name := uname.sysname
-  uname_host := uname.nodename
-  uname_arch := uname.machine
+	uname := os.uname()
+	uname_version := uname.release
+	uname_name := uname.sysname
+	uname_host := uname.nodename
+	uname_arch := uname.machine
 
-  println("OS: ${uname_name} ${uname_arch}")
-  println("Host: ${uname_host}")
-  println("Kernel: ${uname_version}")
+	println('')
+	println('OS: ${uname_name} ${uname_arch}')
+	println('Host: ${uname_host}')
+	println('Kernel: ${uname_version}')
+	println('')
 }
